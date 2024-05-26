097e2d427c6ea2e09fa04b5e41f0688232bf49ee
BS00849 <bs00849@MDs-MacBook-Pro.local>
2024-05-26 18:21:25 +0600

    Riverpod async notifier apply on signin page

--- lib/core/notifiers/controller.dart
+++ lib/core/notifiers/controller.dart
@@ -47,6 +47,6 @@ class Controller extends _$Controller {
       );
     }
   }
-
+  
   //signout
 }
---
+++ lib/core/notifiers/login_button_controller.dart
@@ -0,0 +1,16 @@
+import 'package:demo_ui/core/notifiers/email_controller.dart';
+import 'package:demo_ui/core/notifiers/password_controller.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'login_button_controller.g.dart';
+@riverpod
+class LoginButtonController extends _$LoginButtonController{
+  @override
+  bool build(){
+    return false;
+  }
+  void update(){
+    bool emailProvider = ref.watch(emailControllerProvider);
+    bool passwordProvider = ref.watch(passwordControllerProvider);
+    state = (emailProvider & passwordProvider);
+  }
+}
