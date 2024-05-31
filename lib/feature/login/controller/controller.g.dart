979bb779587aea8ed1997d3952c156fcb58b4e10
BS00849 <bs00849@MDs-MacBook-Pro.local>
2024-05-31 17:31:11 +0600

    Refactor: Code formatting complete

--- lib/feature/login/controller/controller.g.dart
+++ lib/feature/login/controller/controller.g.dart
@@ -11,7 +11,7 @@ String _$signInHash() => r'3da433f207d15efee265407adb3f71b715d39f33';
 /// See also [SignIn].
 @ProviderFor(SignIn)
 final signInProvider =
-    AutoDisposeAsyncNotifierProvider<SignIn, ModelClass?>.internal(
+    AutoDisposeAsyncNotifierProvider<SignIn, LoginModelState?>.internal(
   SignIn.new,
   name: r'signInProvider',
   debugGetCreateSourceHash:
@@ -20,6 +20,6 @@ final signInProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$SignIn = AutoDisposeAsyncNotifier<ModelClass?>;
+typedef _$SignIn = AutoDisposeAsyncNotifier<LoginModelState?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
