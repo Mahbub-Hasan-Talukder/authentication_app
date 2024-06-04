d9e50ad4075bee0fc8696b9db66f36228db6abe5
mahbub-hasan-talukdar <mahbub.hasan@brainstation-23.com>
2024-06-04 16:36:21 +0600

    Refactor: Implement data layer

---
+++ lib/feature/login/domain/entities/sub_entities.dart
@@ -0,0 +1,4 @@
+class User {
+  String firstName, lastName, email;
+  User({required this.firstName, required this.lastName, required this.email});
+}
---
+++ lib/feature/login/domain/repository/login_repository.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+import 'package:authentication_app/feature/login/domain/entities/login_entity.dart';
+
+abstract class LoginRepository {
+  FutureOr<LoginEntity?> getUserLogin(
+      {required String email, required String password});
+}
